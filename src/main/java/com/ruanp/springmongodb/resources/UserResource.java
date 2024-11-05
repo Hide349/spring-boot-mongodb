5870b0fe93046745d0d275ce8242786c49bd0710
Ruan <ruanpablooliveira20@gmail.com>
2024-11-04 22:58:26 -0300

    Getting user by id

--- src/main/java/com/ruanp/springmongodb/resources/UserResource.java
+++ src/main/java/com/ruanp/springmongodb/resources/UserResource.java
@@ -6,8 +6,9 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ruanp.springmongodb.domain.User;
@@ -23,11 +24,18 @@ public class UserResource {
 	private UserService service;
 	
 	// Também pode ser feito com o @GetMapping
-	@RequestMapping(method = RequestMethod.GET)
+	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
 		
 		List<User> list =service.findAll();
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User user = service.findById(id);
+		
+		return ResponseEntity.ok().body(new UserDTO(user));
 	}
 }
--- src/main/java/com/ruanp/springmongodb/services/UserService.java
+++ src/main/java/com/ruanp/springmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.ruanp.springmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.ruanp.springmongodb.domain.User;
 import com.ruanp.springmongodb.repository.UserRepository;
+import com.ruanp.springmongodb.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -18,4 +20,10 @@ public class UserService {
 	public List<User> findAll(){
 		return rep.findAll();
 	}
+	
+	public User findById(String id) {
+		Optional<User> user = rep.findById(id);
+
+		return user.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado!"));
+ 	}
 }
