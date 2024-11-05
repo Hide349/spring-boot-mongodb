899f3ebe4e595c6963e4563a5ab7638a00ca8cfd
Ruan <ruanpablooliveira20@gmail.com>
2024-11-05 12:45:40 -0300

    Referencing user posts

--- src/main/java/com/ruanp/springmongodb/config/Instantiation.java
+++ src/main/java/com/ruanp/springmongodb/config/Instantiation.java
@@ -48,6 +48,9 @@ public class Instantiation implements CommandLineRunner{
 		Post post2 = new Post(null,sdf.parse("21/03/2018"),"Partiu viagem!!","Vou viajar para são paulo!",new AuthorDTO(maria));
 
 		postRepository.saveAll(Arrays.asList(post1,post2));
+		
+		maria.getPosts().addAll(Arrays.asList(post1,post2));
+		userRepository.save(maria);
 
 	}
 
